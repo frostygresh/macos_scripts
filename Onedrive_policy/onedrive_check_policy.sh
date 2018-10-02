6bf0fb21ca3c745892e45298775a4b707d2d81d3
Brian Frost <brianfrost@consolidated.net>
2018-10-02 11:03:53 -0500

    Updates to Onedrive policy

--- Onedrive_policy/onedrive_check_policy.sh
+++ Onedrive_policy/onedrive_check_policy.sh
@@ -1,1 +1,1 @@
-defaults read ~/Library/Containers/com.microsoft.OneDrive-mac/Data/Library/Preferences/com.microsoft.OneDrive-mac.plist DisablePersonalSync
+defaults read ~/Library/Preferences/com.microsoft.OneDrive.plist DisablePersonalSync
--- Onedrive_policy/onedrive_plist_write.sh
+++ Onedrive_policy/onedrive_plist_write.sh
@@ -7,11 +7,13 @@ onedrivePlist="$HOME/Library/Preferences/com.microsoft.OneDrive.plist"
 /usr/libexec/PlistBuddy -c "Add :Tenants:53049b77-3e8f-4792-977f-0a3e5f23891b dict" $onedrivePlist
 /usr/libexec/PlistBuddy -c "Add :Tenants:53049b77-3e8f-4792-977f-0a3e5f23891b:DefaultFolder string 'file:///$HOME/OneDrive/'" $onedrivePlist
 
-onedrivePlist="$HOME/Library/Containers/com.microsoft.OneDrive-mac/Data/Library/Preferences/com.microsoft.OneDrive-mac.plist"
+if [ -f "$HOME/Library/Containers/com.microsoft.OneDrive-mac" ]; then
+  onedrivePlist="$HOME/Library/Containers/com.microsoft.OneDrive-mac/Data/Library/Preferences/com.microsoft.OneDrive-mac.plist"
 
-#/usr/libexec/PlistBuddy -c "Add :HideDockIcon bool False" $onedrivePlist
-/usr/libexec/PlistBuddy -c "Add :DisablePersonalSync bool True" $onedrivePlist
-/usr/libexec/PlistBuddy -c "Add :TeamSiteSyncPreview bool True" $onedrivePlist
-/usr/libexec/PlistBuddy -c "Add :Tenants dict" $onedrivePlist
-/usr/libexec/PlistBuddy -c "Add :Tenants:53049b77-3e8f-4792-977f-0a3e5f23891b dict" $onedrivePlist
-/usr/libexec/PlistBuddy -c "Add :Tenants:53049b77-3e8f-4792-977f-0a3e5f23891b:DefaultFolder string 'file:///$HOME/OneDrive/'" $onedrivePlist
+  #/usr/libexec/PlistBuddy -c "Add :HideDockIcon bool False" $onedrivePlist
+  /usr/libexec/PlistBuddy -c "Add :DisablePersonalSync bool True" $onedrivePlist
+  /usr/libexec/PlistBuddy -c "Add :TeamSiteSyncPreview bool True" $onedrivePlist
+  /usr/libexec/PlistBuddy -c "Add :Tenants dict" $onedrivePlist
+  /usr/libexec/PlistBuddy -c "Add :Tenants:53049b77-3e8f-4792-977f-0a3e5f23891b dict" $onedrivePlist
+  /usr/libexec/PlistBuddy -c "Add :Tenants:53049b77-3e8f-4792-977f-0a3e5f23891b:DefaultFolder string 'file:///$HOME/OneDrive/'" $onedrivePlist
+fi
